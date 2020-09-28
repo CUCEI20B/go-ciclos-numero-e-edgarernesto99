56fae4197b265563886b863e060f16fb5564fe06
Edgar Guzman <edgar.guzman6009@alumnos.udg.mx>
2020-09-28 09:14:57 -0500

    prueba01

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var ( 
+		limit int;
+		total float64 = 1.0;
+		factorial int;
+	)
+
+	fmt.Scan(&limit);
+	factorial = 1;
+
+	for i:=1; i<limit; i++ {
+		factorial = factorial * i;
+		total = total + 1/float64(factorial);
+	}
+	fmt.Println(total);
 }
